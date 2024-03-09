781000f0054b364c9c65f652e1c815b0ed69327c
John Seo <shostga@naver.com>
2024-03-09 23:24:08 +0900

    fifteenth-1 commit

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -76,12 +76,14 @@ export async function updateInvoice(
   prevState: State,
   formData: FormData,
 ) {
+  // 폼 데이터의 필드를 유효성 검사하기 위해 UpdateInvoice 스키마를 사용
   const validatedFields = UpdateInvoice.safeParse({
     customerId: formData.get('customerId'),
     amount: formData.get('amount'),
     status: formData.get('status'),
   });
 
+  // 유효성 검사 실패 시 오류 메시지 반환
   if (!validatedFields.success) {
     return {
       errors: validatedFields.error.flatten().fieldErrors,
@@ -89,21 +91,26 @@ export async function updateInvoice(
     };
   }
 
+  // 유효성 검사가 성공하면 필요한 데이터 추출
   const { customerId, amount, status } = validatedFields.data;
+  // 금액을 센트 단위로 변환
   const amountInCents = amount * 100;
 
   try {
+    // 데이터베이스 업데이트 쿼리 실행
     await sql`
     UPDATE invoices
     SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
     WHERE id = ${id}
   `;
   } catch (error) {
+    // 데이터베이스 오류 시 메시지 반환
     return {
       message: 'Database Error: Failed to Update Invoice.',
     };
   }
 
+  // 경로를 재검증하고 대시보드의 인보이스 페이지로 리다이렉션
   revalidatePath('/dashboard/invoices');
   redirect('/dashboard/invoices');
 }
